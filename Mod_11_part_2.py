2c018fce3aeffb2f263442ac0163f811d31ccba6
HromovAndrey <andrey.gromov.36912@gmail.com>
2024-03-24 21:52:47 +0300

    Перевантаження методів

--- Mod_11_part_2.py
+++ Mod_11_part_2.py
@@ -1,46 +1,11 @@
-#качина типізація
-class Duck:
-    def quack(self):
-        print("Кря")
+class StringManipulator:
+    def concatenate(self, a, b):
+        return a + b
+    def concatenate(self, a, b):
+        return str(a) + str(b)
+calc = StringManipulator()
+print(calc.concatenate(2,3))
+print(calc.concatenate("2", "3"))
 
-class Person:
-    def quack(self):
-         print("Крякає по людськи")
 
-def make_it_quack(obj):
-    obj.quack()
-
-duck = Duck()
-person = Person()
-make_it_quack(duck)
-make_it_quack(person)
-
-#Статичний та динамічний поліморфізм
-
-#статичний поліморфізм
-def add(x, y)
-    return x + y
-result1 = add(1 ,4)
-result2 = add("Hello", "World")
-print(result1, result2)
-
-#динамічний поліморфізм
-class Animal:
-    def make_sound(self):
-        pass
-class Dog(Animal):
-    def make_sound(self):
-        return"Bark!"
-
-class Cat(Animal):
-    def make_sound(self):
-        return "Meow"
-
-def animal_speak(animal):
-    return animal.make_sound()
-
-dog, cat, = Dog(), Cat()
-result1 = animal_speak(dog)
-result2 = animal_speak(cat)
-print(result1, result2)
 
