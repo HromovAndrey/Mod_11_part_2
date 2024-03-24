e5f51b5b987b89ba71d8d1e22e05bba985a97248
HromovAndrey <andrey.gromov.36912@gmail.com>
2024-03-24 21:15:55 +0300

    ачина типизація

---
+++ Mod_11_part_2.py
@@ -0,0 +1,16 @@
+#качина типізація
+class Duck:
+    def quack(self):
+        print("Кря")
+
+class Person:
+    def quack(self):
+         print("Крякає по людськи")
+
+def make_it_quack(obj):
+    obj.quack()
+
+duck = Duck()
+person = Person()
+make_it_quack(duck)
+make_it_quack(person)
