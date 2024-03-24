1c4987a79bcbdc5a5147c27ddf5d4cba303f5b45
HromovAndrey <andrey.gromov.36912@gmail.com>
2024-03-24 22:28:58 +0300

    Завдання 1 перевантаження методів

--- Mod_11_part_2.py
+++ Mod_11_part_2.py
@@ -1,12 +1,43 @@
-class ComplexNumber:
-    def __init__(self, real, imag):
-        self.real = real
-        self.imag = imag
+#Завдання 1
+# Створіть клас «Дріб». Збережіть у класі чисельник
+#та знаменник. Реалізуйте методи класу для введеннявиведення даних. Також створіть методи класу для
+#виконання арифметичних операцій (додавання,
+#віднімання, множення, ділення і т. д.). До вже
+#реалізованого класу «Дріб» додайте необхідні
+#перевантажені методи та оператори.
+class Fraction:
+    def __init__(self, numerator, denominator):
+        self.numerator = numerator
+        self.denominator = denominator
 
-    def __add__(self,other):
-        return ComplexNumber(self.real + other.real, self.imag + other.imag)
+    def __str__(self):
+        return f"{self.numerator}/{self.denominator}"
 
-num1 = ComplexNumber(1, 2)
-num2 = ComplexNumber(3 ,4)
-result = num1 + num2
-print(result)
+    def __add__(self, other):
+            new_numerator = self.numerator * other.denominator + other.numerator * self.denominator
+            new_denominator = self.denominator * other.denominator
+            return Fraction(new_numerator, new_denominator)
+
+    def __sub__(self, other):
+        new_numerator = self.numerator * other.denominator - other.numerator * self.denominator
+        new_denominator = self.denominator * other.denominator
+        return Fraction(new_numerator, new_denominator)
+
+    def __mul__(self, other):
+        new_numerator = self.numerator * other.numerator
+        new_denominator = self.denominator * other.denominator
+        return Fraction(new_numerator, new_denominator)
+
+    def __truediv__(self, other):
+        new_numerator = self.numerator * other.denominator
+        new_denominator = self.denominator * other.numerator
+        return Fraction(new_numerator, new_denominator)
+
+fraction1 = Fraction(1, 2)
+fraction2 = Fraction(3, 4)
+
+print(fraction1 + fraction2)
+print(fraction1 - fraction2)
+print(fraction1 * fraction2)
+print(fraction1 / fraction2)
+
