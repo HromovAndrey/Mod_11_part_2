8863c177980f460dc383a27b86aafe117724fee0
HromovAndrey <andrey.gromov.36912@gmail.com>
2024-03-24 22:39:52 +0300

    Завдання 2 модуль 11 часть 2

--- Mod_11_part_2.py
+++ Mod_11_part_2.py
@@ -1,43 +1,54 @@
-#Завдання 1
-# Створіть клас «Дріб». Збережіть у класі чисельник
-#та знаменник. Реалізуйте методи класу для введеннявиведення даних. Також створіть методи класу для
-#виконання арифметичних операцій (додавання,
-#віднімання, множення, ділення і т. д.). До вже
-#реалізованого класу «Дріб» додайте необхідні
-#перевантажені методи та оператори.
-class Fraction:
-    def __init__(self, numerator, denominator):
-        self.numerator = numerator
-        self.denominator = denominator
+#Завдання 2
+# Реалізуйте клас «Стадіон». Збережіть у класі: назву
+#стадіону, дату відкриття, країну, місто, місткість. Реалізуйте
+#методи класу для введення-виведення даних та інших
+#операцій. До вже реалізованого класу «Стадіон» додайте
+#необхідні перевантажені методи та оператори.
+
+class Stadium:
+    def __init__(self, name, opening_date, country, city, capacity):
+        self.name = name
+        self.opening_date = opening_date
+        self.country = country
+        self.city = city
+        self.capacity = capacity
+
+    def display_info(self):
+        print("Стадіон:", self.name)
+        print("Дата відкриття:", self.opening_date)
+        print("Країна:", self.country)
+        print("Місто:", self.city)
+        print("Місткість:", self.capacity)
 
     def __str__(self):
-        return f"{self.numerator}/{self.denominator}"
+        return f"Стадіон {self.name} розташований у місті {self.city}, {self.country}. Відкритий {self.opening_date}. Має місткість {self.capacity} осіб."
+
+    def __eq__(self, other):
+        return self.name == other.name and self.city == other.city
+
+    def __lt__(self, other):
+        return self.capacity < other.capacity
 
     def __add__(self, other):
-            new_numerator = self.numerator * other.denominator + other.numerator * self.denominator
-            new_denominator = self.denominator * other.denominator
-            return Fraction(new_numerator, new_denominator)
+        total_capacity = self.capacity + other.capacity
+        return Stadium(f"{self.name} & {other.name}", "Порожній", "Сума", "Сума", total_capacity)
 
-    def __sub__(self, other):
-        new_numerator = self.numerator * other.denominator - other.numerator * self.denominator
-        new_denominator = self.denominator * other.denominator
-        return Fraction(new_numerator, new_denominator)
 
-    def __mul__(self, other):
-        new_numerator = self.numerator * other.numerator
-        new_denominator = self.denominator * other.denominator
-        return Fraction(new_numerator, new_denominator)
+stadium1 = Stadium("Camp Nou", "24.09.1957", "Іспанія", "Барселона", 99354)
+stadium2 = Stadium("Santiago Bernabeu", "14.12.1947", "Іспанія", "Мадрид", 81044)
 
-    def __truediv__(self, other):
-        new_numerator = self.numerator * other.denominator
-        new_denominator = self.denominator * other.numerator
-        return Fraction(new_numerator, new_denominator)
+stadium1.display_info()
+print()
 
-fraction1 = Fraction(1, 2)
-fraction2 = Fraction(3, 4)
+print(stadium2)
+print()
 
-print(fraction1 + fraction2)
-print(fraction1 - fraction2)
-print(fraction1 * fraction2)
-print(fraction1 / fraction2)
+print("Чи рівні стадіони?", stadium1 == stadium2)
+print()
+
+print("Стадіон з більшою місткістю:", max(stadium1, stadium2).name)
+print()
+
+merged_stadium = stadium1 + stadium2
+print("Об'єднаний стадіон:", merged_stadium)
 
